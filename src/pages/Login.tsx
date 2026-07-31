612b7518e723d43847df0775902f7da8589ed842
Daniyal <thedaniiyal@gmail.com>
2026-07-31 16:58:27 +0530

    Refactor login page UI with structured layout and improved styles

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,15 +2,32 @@ import "./Login.css";
 
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login-page">
+            <div className="login-card">
+                <h1 className="login-heading">Login</h1>
 
-            <input type="email" placeholder ="Email" />
-            <br />
-            
-            <input type="password" placeholder="Password"/>
-            <br />
-            <button>Login</button>
+                <label htmlFor="email" className="login-label">Email</label>
+                <input id="email" className="login-input" type="email" placeholder="Enter your Email" />
+
+                <label htmlFor="password" className="login-label">Password</label>
+                <input id="password" className="login-input" type="password" placeholder="Enter your Password" />
+
+                <a className="forgot-password" href="#">Forgot Password?</a>
+
+                <button className="login-button">Login</button>
+
+                <div className="signup-section">
+                    <p className="signup-text">
+                        Don't have an account?
+
+                        <a href="#" className="signup-link">
+                            Sign Up
+                        </a>
+                    </p>
+                </div>
+            </div>
+
+
         </div>
     )
 }
